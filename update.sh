92bc8d37937505144f655d4b7b1008fda641b79c
Brian Zalewski <brian@enviedsolutions.com>
2021-03-06 22:11:12 +0000

    Update files/package.json, update.sh, files/.husky/pre-commit files

--- update.sh
+++ update.sh
@@ -4,45 +4,51 @@
 # installed and then copies over base files from the modules. It also generates the
 # documentation.
 
-set -e
+set -ex
 
-if [ ! -d "./modules/bento" ]; then
-  cd modules
-  git submodule add -b master https://github.com/chef/bento.git
-  cd ..
+# Ensure submodules are present and up-to-date
+if [ ! -d "./.modules/bento" ]; then
+  git submodule add -b master https://github.com/chef/bento.git ./.modules/bento
 else
-  cd modules/bento
+  cd ./.modules/bento
   git checkout master && git pull origin master
   cd ../..
 fi
-if [ ! -d "./modules/docs" ]; then
-  cd modules
-  git submodule add -b master https://gitlab.com/megabyte-space/documentation/packer.git docs
-  cd ..
+if [ ! -d "./.modules/docs" ]; then
+  git submodule add -b master https://gitlab.com/megabyte-space/documentation/packer.git ./.modules/docs
 else
-  cd modules/docs
+  cd ./.modules/docs
   git checkout master && git pull origin master
   cd ../..
 fi
-if [ ! -d "./modules/shared" ]; then
-  cd modules
-  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git
-  cd ..
+if [ ! -d "./.modules/shared" ]; then
+  git submodule add -b master https://gitlab.com/megabyte-space/common/shared.git ./.modules/shared
 else
-  cd modules/shared
+  cd ./.modules/shared
   git checkout master && git pull origin master
   cd ../..
 fi
-cp -rf ./modules/shared/.github .
-cp -rf ./modules/shared/.gitlab .
-cp -rf ./modules/shared/.vscode .
-cp ./modules/shared/.cspell.json .cspell.json
-cp ./modules/shared/.editorconfig .editorconfig
-cp ./modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
-cp -Rf ./modules/packer/files/ .
+
+# Copy over files from the shared submodule
+cp -Rf ./.modules/shared/.github .
+cp -Rf ./.modules/shared/.gitlab .
+cp -Rf ./.modules/shared/.vscode .
+cp ./.modules/shared/.editorconfig .editorconfig
+cp ./.modules/shared/CODE_OF_CONDUCT.md CODE_OF_CONDUCT.md
+
+# Copy files over from the Packer shared submodule
+PACKAGE_VERSION=$(cat package.json | jq '.version')
+cp -Rf ./.modules/packer/files/ .
+jq --arg a "${PACKAGE_VERSION}" '.version = $a' package.json > __jq.json && mv __jq.json package.json
+npx prettier-package-json --write
+
+# Ensure the pre-commit hook is executable
 chmod 755 .husky/pre-commit
-jq -s '.[0] * .[1]' template.json ./modules/docs/common.json > __bp.json | true
-npx -y @appnest/readme generate --config __bp.json --input ./modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
-npx -y @appnest/readme generate --config __bp.json --input ./modules/docs/blueprint-readme.md | true
+
+# Generate the documentation
+jq -s '.[0] * .[1]' template.json ./.modules/docs/common.json > __bp.json | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-contributing.md --output CONTRIBUTING.md | true
+npx -y @appnest/readme generate --config __bp.json --input ./.modules/docs/blueprint-readme.md | true
 rm __bp.json
-echo "Done updating meta files and generating documentation"
+
+echo "*** Done updating meta files and generating documentation ***"
