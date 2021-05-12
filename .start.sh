41b3f511cd9523340438cc84ce926ec15c5cfa1d
Brian Zalewski <brian@megabyte.space>
2021-05-12 01:23:36 +0000

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -6,7 +6,10 @@
 
 set -e
 
-curl -sL https://git.io/_has | bash -s docker git jq node npm wget
+if [ "$container" != 'docker' ]; then
+  curl -sL https://git.io/_has | bash -s docker git jq node npm wget
+fi
+
 export REPO_TYPE=packer
 git submodule update --init --recursive
 if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
