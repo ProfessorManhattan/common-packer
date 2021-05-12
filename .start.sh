572e2de97e2d4c0790b6951a95876f1350ee66cc
Brian Zalewski <brian@megabyte.space>
2021-05-12 23:22:19 +0000

    Update update.sh, .start.sh files

--- .start.sh
+++ .start.sh
@@ -13,10 +13,10 @@ fi
 export REPO_TYPE=packer
 git submodule update --init --recursive
 if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
-  git submodule add -b master --depth 1 https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
+  git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
 else
   cd ./.modules/$REPO_TYPE
-  git checkout master && git pull --depth 1 --rebase origin master --allow-unrelated-histories
+  git checkout master && git pull origin master
   cd ../..
 fi
 bash ./.modules/$REPO_TYPE/update.sh
