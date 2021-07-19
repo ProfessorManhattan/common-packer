9b6f40732f2643a8ee7df130e9a520ce97010e17
Brian Zalewski <brian@megabyte.space>
2021-07-19 20:13:53 +0000

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-# This script performs maintenance on Packer repositories. It ensures git submodules are
-# installed and then copies over base files from the modules. It also generates the
-# documentation and performs other miscellaneous tasks.
+# @file .start.sh
+# @brief Ensures the project is up-to-date with the latest upstream changes
+# @description
+#   This script performs maintenance on this repository. It ensures the `.common` submodule is
+#   cloned and up-to-date. The `.common` submodule contains files that are shared between projects
+#   that are similar to this one. It also ensures the repository contains the latest code. This
+#   script calls the script in `.common/update.sh` which will make sure the project is
+#   bootstrapped. `bash .start.sh` is the first command you should run when working with this project.
 
 set -e
-
-# shellcheck disable=SC2154
-if [ "$container" != 'docker' ]; then
-  curl -sL https://git.io/_has | bash -s git jq node npm packer vagrant wget
-fi
-
 export REPO_TYPE=packer
-git pull origin master --ff-only
-git submodule update --init --recursive
-if [ ! -f "./.modules/${REPO_TYPE}/update.sh" ]; then
-  git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
-else
-  cd ./.modules/$REPO_TYPE
-  git checkout master && git pull origin master --ff-only
-  cd ../..
+
+if [ -d .git ]; then
+  git pull origin master --ff-only
+  git submodule update --init --recursive
 fi
-bash ./.modules/$REPO_TYPE/update.sh
+
+if [ ! -d .common ]; then
+  git submodule add -b master "https://gitlab.com/megabyte-space/common/$REPO_TYPE.git" ".common"
+else
+  cd .common
+  git checkout master && git pull origin master --ff-only
+  cd ..
+fi
+
+bash .common/update.sh
